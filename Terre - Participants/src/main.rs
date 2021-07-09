b6ec871484ba2f144441f2da98072b9c666f7d71
jarod25 <63459117+jarod25@users.noreply.github.com>
2021-07-09 14:17:40 +0200

    Update main.rs

--- Terre - Participants/src/main.rs
+++ Terre - Participants/src/main.rs
@@ -13,20 +13,20 @@ use help::parse_second_input;
 mod help;
 
 fn generate_board(size: usize) -> DoubleArray {
-    unimplemented!();
+
 }
 pub fn lose(board: &DoubleArray, coords: &Vec<usize>) -> bool {
-    unimplemented!();
+
 }
 
 pub fn get_cactus_number(board_solution: &DoubleArray) -> u16 {
-    unimplemented!();
+
 }
 
 pub fn win(board: &DoubleArray, board_solution: &DoubleArray, cactus_number: u16) -> bool {
-    unimplemented!();
+
 }
 
 pub fn main() {
-    unimplemented!();
+
 }
